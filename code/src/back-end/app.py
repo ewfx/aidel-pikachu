b51876776ef4d7336fa13070d58465d84005e6af
Keerthana S <keerthana07sri@gmail.com>
2025-03-25 17:56:55 +0530

    New Income functionality addition

--- code/src/back-end/app.py
+++ code/src/back-end/app.py
@@ -1,10 +1,19 @@
-from flask import Flask
-from anomaly_detection_income import fetch_income_anomaly
+from flask import Flask, jsonify, request
+from flask_cors import *
+from anomaly_detection_income import fetch_income_anomaly, check_new_income_anomaly
 app = Flask(__name__)
 
-@app.route("/income/anomaly")
+CORS(app)
+@app.route("/income/anomaly", methods=['GET'])
 def home():
-    return fetch_income_anomaly()
+    return jsonify(fetch_income_anomaly())
+
+@app.route("/income/new/anomaly", methods=['POST'])
+def anomaly():
+    data = request.json
+    new_income = data.get('new_income')
+    return jsonify(check_new_income_anomaly(new_income))
+
 
 if __name__ == '__main__':
     # run() method of Flask class runs the application 
